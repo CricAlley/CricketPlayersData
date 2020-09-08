2a841b3c34b35b9406fa1203145e31e0f50cc4b4
Narender Singh Shekhawat <NShekhwat@evolenthealth.com>
2020-09-08 12:46:44 +0530

    d

--- CricketPlayersData/test.sh
+++ CricketPlayersData/test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 InputFolderPath=$1
 PrefixPath=$2
-if [ -d "$PrefixPath/$InputFolderPath" ]; then echo "I am not noob"; else echo "i am noob"; fi
+if [ -d "$PrefixPath/$InputFolderPath" ]; then sudo rmdir -R "$HOME/$InputFolderPath"; fi
+sudo mkdir -p $PrefixPath/$InputFolderPath
+sudo chmod 777 $PrefixPath/$InputFolderPath
