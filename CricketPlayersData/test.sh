c9fe62758ae55cbcd8a206c4854a48cc6c47089d
Narender Singh Shekhawat <NShekhwat@evolenthealth.com>
2020-09-08 12:42:16 +0530

    vb

--- CricketPlayersData/test.sh
+++ CricketPlayersData/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 InputFolderPath=$1
 PrefixPath=$2
-if [ -d "$PrefixPath/$InputFolderPath" ]; then echo "Path exists"; fi
+if [ -d "$PrefixPath/$InputFolderPath" ]; then echo "I am not noob"; else echo "i am noob"; fi
