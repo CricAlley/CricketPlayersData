33d65b817457707704cbb5f3d1a402d6467757f9
Narender Singh Shekhawat <NShekhwat@evolenthealth.com>
2020-09-08 13:36:31 +0530

    dfdf

--- CricketPlayersData/test.sh
+++ CricketPlayersData/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 InputFolderPath=$1
 PrefixPath=$2
-if [[ -d "$PrefixPath/$InputFolderPath" ]]; then sudo rm -d -R "$HOME/$InputFolderPath"; fi;
+if [[ -d "$PrefixPath/$InputFolderPath" ]]; then sudo rm -d -R "$PrefixPath/$InputFolderPath"; fi;
 sudo mkdir -p "$PrefixPath/$InputFolderPath"
 sudo chmod 777 "$PrefixPath/$InputFolderPath/"
