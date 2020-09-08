7b9ed1415f2de7a5fbcf5c7c606023e85c4e3eb9
Narender Singh Shekhawat <NShekhwat@evolenthealth.com>
2020-09-08 14:35:22 +0530

    gg

--- CricketPlayersData/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-InputFolderPath="$1"
-PrefixPath="$2"
-if [[ -d "$PrefixPath/$InputFolderPath" ]]; then sudo rm -d "$PrefixPath/$InputFolderPath"; fi;
-sudo mkdir -p "$PrefixPath/$InputFolderPath"
-sudo chmod -R 777 "$PrefixPath/$InputFolderPath"
