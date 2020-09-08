9d7a58678c567f398070b65a7c01358de5b33b1f
Narender Singh Shekhawat <NShekhwat@evolenthealth.com>
2020-09-08 12:20:38 +0530

    f

--- CricketPlayersData/test.sh
+++ CricketPlayersData/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 InputFolderPath=$1
 PrefixPath=$2
-if[[ -d "$PrefixPath/$InputFolderPath" ]]
+if [[ -d "$PrefixPath/$InputFolderPath" ]]
 then
 	echo "Path exists"
 else
 	echo "Path does not exists."
-fi
+fi;
