fda7756e36aed86b5dd45899a0f991c9b54d5cdd
narender <naru1790@gmail.com>
2020-09-08 22:22:03 +0530

    df

--- CricketPlayersData/tomato.sh
+++ CricketPlayersData/tomato.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-inputfolderpath=$1
-prefixpath=$2
-# if [ -d $(prefixpath)/$(inputfolderpath) ]; then sudo rm -d -r $(prefixpath)/$(inputfolderpath); fi;
-sudo mkdir -p "$prefixpath//$inputfolderpath"
-sudo chmod 777 "$prefixpath//$inputfolderpath"
+inputfolderpath="$1"
+prefixpath="$2"
+sudo mkdir -p "$prefixpath/$inputfolderpath"
+sudo chmod 777 "$prefixpath/$inputfolderpath"
